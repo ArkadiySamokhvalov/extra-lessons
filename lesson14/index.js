c2d699eb878ef7b4c59b89b5950b3d5962d785be
Arkadiy <arkadiy.samokhvalov@bk.ru>
2020-02-11 21:12:34 +0300

    14

---
+++ lesson14/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+class DomElement {
+  constructor(selector, text = '', height = '', width = '', bg = '', fontSize = '') {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.text = text;
+  }
+  createElement() {
+    let element = '';
+    let arr = [];
+    if (this.selector[0] === '.') {
+      element = document.createElement('div');
+      element.className = this.selector.substr(1);
+    } else if (this.selector[0] === '#') {
+      element = document.createElement('div');
+      element.id = this.selector.substr(1);
+    } else if (this.selector.split('.')) {
+      arr = this.selector.split('.');
+      element = document.createElement(arr[0]);
+      element.className = arr[1];
+    } else if (this.selector.split('#')) {
+      arr = this.selector.split('#');
+      element = document.createElement(arr[0]);
+      element.id = arr[1];
+    } else {
+      element = document.createElement(this.selector);
+    }
+
+    let cssText = '';
+    cssText += (this.height !== '') ? 'height: ' + this.height + '; ' : '';
+    cssText += (this.width !== '') ? 'width: ' + this.width + '; ' : '';
+    cssText += (this.bg !== '') ? 'background: ' + this.bg + '; ' : '';
+    cssText += (this.fontSize !== '') ? 'font-size: ' + this.fontSize + ';' : '';
+
+    if (cssText !== '') {
+      element.style.cssText = cssText;
+    }
+    if (this.text !== '') {
+      element.textContent = this.text ;
+    }
+
+    return element;
+  }
+}
+
+let obj = new DomElement('span.text', 'text');
+let el = obj.createElement();
+document.body.appendChild(el);
+
+let obj2 = new DomElement('#block', 'block', '500px', '350px', 'red', '35px');
+let el2 = obj2.createElement();
+document.body.appendChild(el2);
